c0304f57a87e509f66b69c39e1048c7fda9d4ab0
mdayann <mdayan312@gmail.com>
2021-01-25 00:29:06 +0700

    Set up boilerplate

---
+++ src/components/Chat.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Chat = () => <h1>Chat</h1>;
+
+export default Chat;
---
+++ src/components/Join.js
@@ -0,0 +1,10 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Join = () => {
+  return <h1>Hello</h1>;
+};
+
+export default Join;
